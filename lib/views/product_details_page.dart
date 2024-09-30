39700c9e0277f947c23c3da52407a95adaa4fc6f
muzammil-crypto <muzamilrafiq000@gmail.com>
2024-09-30 18:19:50 +0500

    dark light theme added

--- lib/views/product_details_page.dart
+++ lib/views/product_details_page.dart
@@ -1,3 +1,5 @@
+import 'package:deferred_comps/app_colors/app_colors.dart';
+import 'package:deferred_comps/widgets/custom_primary_button.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailsPage extends StatelessWidget {
@@ -22,23 +24,27 @@ class ProductDetailsPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(imageUrl),
-            const SizedBox(height: 16),
-            Text(
-              title,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              '\$$price',
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            const SizedBox(height: 16),
-            Text(description),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(imageUrl),
+              const SizedBox(height: 16),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                '\$$price',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              const SizedBox(height: 16),
+              Text(description),
+              const SizedBox(height: 32),
+              CustomPrimaryButton(text: "Add to cart", onPressed: () {}, color: primaryColor),
+            ],
+          ),
         ),
       ),
     );
